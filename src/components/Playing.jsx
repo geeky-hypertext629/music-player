4bcc46e54abaef9b890d8a4e0ee34a093c37717d
geeky-hypertext629 <107639960+geeky-hypertext629@users.noreply.github.com>
2023-01-01 11:19:16 +0530

    hehe

--- src/components/Playing.jsx
+++ src/components/Playing.jsx
@@ -7,66 +7,110 @@ import song4 from "./stereo.mp3"
 import song5 from "./oned.mp3"
 
 
-const songsList=[song1,song2,song3,song4,song5];
-let i=0;
+const songsList = [song1, song2, song3, song4, song5];
+const timeofsong=[4.06,4.22,3.03,2.33,4.00];
+let i = 0;
+let x=0;
 let audio = new Audio(songsList[i]);
 
+let progress=0;
 export default function Playing(props) {
 
     const [status, setStatus] = useState(false);
+    const [timeline,settimeline] = useState(0);
+    const [currvolume, setvolume] = useState(1);
 
-
-    function playMusic(){
-        if(status===false)
-        {
+    function playMusic() {
+        if (status === false) {
             audio.play();
+            progress=setInterval(()=>{
+                x=x+(100/(timeofsong[i]*60));
+                settimeline(x);
+            },1000);
         }
-        else
-        {
+        else {
             audio.pause();
+            clearInterval(progress);
         }
+
         setStatus(!status);
         props.songNo(i);
     }
-    
-    
-    function playNext(){
+
+
+    function playNext() {
         audio.pause();
         setStatus(false);
-        i=(i+1)%5;
-        audio=new Audio(songsList[i]);
+
+        i = (i + 1) % 5;
+        x=0;
+        settimeline(0);
+        progress=setInterval(()=>{
+            x=x+(100/(timeofsong[i]*60));
+            settimeline(x);
+        },1000);
+        audio = new Audio(songsList[i]);
         audio.play();
         setStatus(true);
         props.songNo(i);
-        
+
     }
-    
-    function playPrev(){
+
+    function playPrev() {
         audio.pause();
         setStatus(false);
-        i=i-1;
-        if(i<0)
-        i=4;
-        audio=new Audio(songsList[i]);
+
+        i = i - 1;
+        if (i < 0)
+            i = 4;
+        x=0;
+        settimeline(0);
+        progress=setInterval(()=>{
+            x=x+(100/(timeofsong[i]*60));
+            settimeline(x);
+        },1000);
+        audio = new Audio(songsList[i]);
         audio.play();
         setStatus(true);
         props.songNo(i);
     }
+    function volumeTrigger(e) {
+        setvolume(e.target.value);
+        audio.volume = currvolume / 100;
+    }
 
 
-  return (
-    <div className='play-section'>
-    <div className="controls">
-    <button className="previous" onClick={playPrev}>
-    <i class="fa-solid fa-backward"></i>
-    </button>
-    <button className="play" onClick={playMusic}>
-    <i class={status ? "fa-solid fa-pause" : "fa-solid fa-play"}></i>
-    </button>
-    <button className="next" onClick={playNext}>
-    <i class="fa-sharp fa-solid fa-forward"></i>
-    </button>
-    </div>
-    </div>
-  )
+
+    function updatetimeline(e){
+        settimeline(e.target.value);
+        console.log(timeline);
+    }
+
+    return (
+        <div className='play-section'>
+            <div className="controls">
+            <div>
+                <button className="previous" onClick={playPrev}>
+                    <i class="fa-solid fa-backward"></i>
+                </button>
+                <button className="play" onClick={playMusic}>
+                    <i className={status ? "fa-solid fa-pause" : "fa-solid fa-play"}></i>
+                </button>
+                <button className="next" onClick={playNext}>
+                    <i class="fa-sharp fa-solid fa-forward"></i>
+                </button>
+                </div>
+                <div className="progressbar">
+                <p>0:00</p>
+                    <input type="range" min="0" max="100" value={timeline} onChange={updatetimeline}/>
+                    <p>{timeofsong[i]}</p>
+                </div>
+            </div>
+            <div className='volume'>
+                <p>Volume</p>
+                <input type="range" id="volume" name="volume" 
+                    min="0" max="100" value={currvolume} onChange={volumeTrigger} />
+            </div>
+        </div>
+    )
 }
